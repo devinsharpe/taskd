f6e045e860a885a44d01582d5b9759f7fb769f63
Devin Sharpe <devinrsharpe@gmail.com>
2021-12-31 20:13:56 -0500

    feat(env): Added properties to .env.local file and defined them in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_SUPABASE_URL: string;
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    }
+  }
+}
+
 declare module "@iconscout/react-unicons" {
   export interface UniconProps {
     color?: string;
