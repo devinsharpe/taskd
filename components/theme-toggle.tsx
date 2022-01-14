bd4aee0cfdd5b788645d14110c6086e105e5c020
Devin Sharpe <devinrsharpe@gmail.com>
2022-01-13 22:09:38 -0500

    refactor(frontend): Refactored dark mode system into store based theme system

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { UilMoon, UilSun } from "@iconscout/react-unicons";
+
+import { motion } from "framer-motion";
+import { useThemeStore } from "../store";
+
+interface ThemeToggleProps {
+  isHidden?: boolean;
+}
+
+const ThemeToggle: React.FC<ThemeToggleProps> = ({ isHidden }) => {
+  const { theme, checkTheme, setTheme } = useThemeStore((state) => ({
+    theme: state.theme,
+    checkTheme: state.checkTheme,
+    setTheme: state.setTheme,
+  }));
+
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    setTheme(checkTheme());
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", (e) => {
+        setTheme(e.matches ? "dark" : "light");
+      });
+  }, [setTheme, checkTheme]);
+
+  return (
+    <>
+      {isHidden ? (
+        <></>
+      ) : (
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          className="p-2 alternate"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? <UilMoon /> : <UilSun />}
+        </motion.button>
+      )}
+    </>
+  );
+};
+
+ThemeToggle.defaultProps = {
+  isHidden: false,
+};
+
+export default ThemeToggle;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,46 +1,71 @@
 import React, { useEffect, useState } from "react";
 import { useToastStore, useUserStore } from "../store";
 
+import { Account } from "../models";
 import AccountDetailsForm from "../components/account-details-form";
 import ActionBar from "../components/action-bar";
 import Button from "../components/button";
 import Modal from "../components/modal";
 import type { NextPage } from "next";
 import { UilInfo } from "@iconscout/react-unicons";
+import { supabase } from "../utils/supabaseClient";
 
 const Home: NextPage = () => {
-  const { account, user } = useUserStore((state) => ({
+  const { account, setAccount, user } = useUserStore((state) => ({
     account: state.account,
+    setAccount: state.setAccount,
     user: state.user,
   }));
-  const [showActionBar, setShowActionBar] = useState(false);
+  const [isSavingAccount, setIsSavingAccount] = useState(false);
+  const [showActionBar, setShowActionBar] = useState(true);
   const { pushToast } = useToastStore((state) => ({ pushToast: state.push }));
 
-  const handleAccountFormSubmit = async (data: {
+  const handleAccountFormSubmit = async (formData: {
     lastName: string;
     firstName: string;
     togglToken: string;
   }) => {
-    console.log(data);
+    if (user) {
+      setIsSavingAccount(true);
+      const { data, error } = await supabase.from<Account>("accounts").insert({
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: user!.email,
+        togglToken: formData.togglToken,
+        user: user!.id,
+      });
+      if (error) {
+        pushToast({
+          title: "Something went wrong",
+          description: error.message,
+          duration: 10000,
+          isClosable: true,
+          status: "ERROR",
+        });
+      } else {
+        setAccount(data![0]);
+      }
+      setIsSavingAccount(true);
+    }
   };
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowActionBar(true);
-    }, 5000);
-  }, []);
 
   return (
     <div>
-      {showActionBar && <ActionBar />}
-      {account || (
+      {account ? <ActionBar /> : <></>}
+      {account ? (
+        <></>
+      ) : (
         <Modal
           title="User Details"
           isClosable={false}
           isOpen={true}
           onClose={() => console.log("close")}
         >
-          <AccountDetailsForm user={user!} onSubmit={handleAccountFormSubmit} />
+          <AccountDetailsForm
+            user={user!}
+            onSubmit={handleAccountFormSubmit}
+            isLoading={isSavingAccount}
+          />
         </Modal>
       )}
     </div>
