7a9a6e721fd1f340a54566d7a7fd6731dcbeca94
Devin Sharpe <devinrsharpe@gmail.com>
2022-01-22 20:38:29 -0500

    feat(env): Added NEXT_PUBLIC_APP_NAME environment variable

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NEXT_PUBLIC_APP_NAME: string;
       NEXT_PUBLIC_SUPABASE_URL: string;
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
     }
